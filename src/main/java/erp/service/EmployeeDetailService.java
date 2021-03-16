e537f2e7ff447b9cdc043c65b162289ee39e1da0
inhwanK <inhan1009@naver.com>
2021-03-16 19:48:46 +0900

    EmployeeDetailService 구현. 이제 편히 자면됨.

--- src/main/java/erp/service/EmployeeDetailService.java
+++ src/main/java/erp/service/EmployeeDetailService.java
@@ -1,5 +1,28 @@
 package erp.service;
 
+import java.util.List;
+
+import erp.dao.EmployeeDetailDao;
+import erp.dao.impl.EmployeeDetailDaoImpl;
+import erp.dto.EmpDetail;
+import erp.dto.Employee;
+
 public class EmployeeDetailService {
+	private EmployeeDetailDao empDetailDao = EmployeeDetailDaoImpl.getInstance();
 	
+	public EmpDetail showEmployeeDetail(Employee employee){
+		return empDetailDao.selectEmployeeDetailByNo(employee);
+	}
+	
+	public void modifyEmployeeDetail(EmpDetail empDetail) {
+		empDetailDao.updateEmployeeDetail(empDetail);
+	}
+	
+	public void addEmployeeDetail(EmpDetail empDetail) {
+		empDetailDao.insertEmployeeDetail(empDetail);
+	}
+	
+	public void removeEmployeeDetail(Employee employee) {
+		empDetailDao.deleteEmployeeDetail(employee);
+	}
 }
